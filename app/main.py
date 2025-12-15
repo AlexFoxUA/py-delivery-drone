6e569d701f1f9c445f02d19854cbb807f737397f
Oleksandr Vilenskiy <128734566+AlexFoxUA@users.noreply.github.com>
2025-12-15 11:23:26 +0200

    Robot 2

--- app/main.py
+++ app/main.py
@@ -13,19 +13,19 @@ class BaseRobot:
         self.weight = weight
         self.coords = (coords or [0, 0]).copy()
 
-    def go_forward(self, step: int = 1):
+    def go_forward(self, step: int = 1) -> None:
         self.coords[1] += step
 
-    def go_back(self, step: int = 1):
+    def go_back(self, step: int = 1) -> None:
         self.coords[1] -= step
 
-    def go_right(self, step: int = 1):
+    def go_right(self, step: int = 1) -> None:
         self.coords[0] += step
 
-    def go_left(self, step: int = 1):
+    def go_left(self, step: int = 1) -> None:
         self.coords[0] -= step
 
-    def get_info(self):
+    def get_info(self) -> str:
         return f"Robot: {self.name}, Weight: {self.weight}"
 
 
@@ -46,10 +46,10 @@ class FlyingRobot(BaseRobot):
 
         self.coords = coords3.copy()
 
-    def go_up(self, step=1):
+    def go_up(self, step=1) -> None:
         self.coords[2] += step
 
-    def go_down(self, step=1):
+    def go_down(self, step=1) -> None:
         self.coords[2] -= step
 
 
@@ -72,9 +72,9 @@ class DeliveryDrone(FlyingRobot):
         if current_load is not None:
             self.hook_load(current_load)
 
-    def hook_load(self, cargo: Cargo):
+    def hook_load(self, cargo: Cargo) -> None:
         if self.current_load is None and cargo.weight <= self.max_load_weight:
             self.current_load = cargo
 
-    def unhook_load(self):
+    def unhook_load(self) -> None:
         self.current_load = None
